8014ab3fd58002c00606bfd041c9c38198a75686
infinitefutureleadersconsultin-star <infinitefutureleadersconsultin@gmail.com>
2025-09-18 01:17:45 -0400

    Create _cors.js

---
+++ api/_cors.js
@@ -0,0 +1,17 @@
+// api/_cors.js
+import cors from "cors";
+
+const corsMiddleware = cors({
+  origin: true,                // reflect request origin
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"],
+});
+
+export function runCors(req, res) {
+  return new Promise((resolve, reject) => {
+    corsMiddleware(req, res, (result) => {
+      if (result instanceof Error) return reject(result);
+      resolve(result);
+    });
+  });
+}
